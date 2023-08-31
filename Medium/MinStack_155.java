da941c4e735076903363b5c798f58869c2f8591a
Sidhant S <sidhantks@gmail.com>
2023-08-31 10:59:57 -0400

    kth smallest

---
+++ Medium/MinStack_155.java
@@ -0,0 +1,27 @@
+public class MinStack_155 {
+
+    private LinkedList<Integer> stack;
+    private LinkedList<Integer> minQueue;
+     public MinStack() {
+
+        stack = new LinkedList<>();
+        minQueue = new LinkedList<>();
+        
+    }
+    
+    public void push(int val) {
+        
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+        
+    }
+    
+    public int getMin() {
+        
+    }
+}
